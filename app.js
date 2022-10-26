ab42f622d3780a6105089252d6ce72a5966fa4ac
Mauro <mauro.mca79@gmail.com>
2022-10-26 17:49:00 -0300

    nova linha no app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Fiz esse commite por conta própria")
+console.log("Enviando para commit para o github")
