18266dcabadca444a4a2770a2c27e23ee5ecf3b0
Mauro <mauro.mca79@gmail.com>
2022-10-26 18:05:35 -0300

    Voltando arquivo js para forma anterior

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-console.log("Fiz esse commite por conta própria")
-console.log("Enviando para commit para o github")
